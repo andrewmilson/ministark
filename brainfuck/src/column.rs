a9e2f5cb478e1dc1971aa6fd9fddfc0e511f7fac
Andrew Milson <andrew.j.milson@gmail.com>
2022-10-18 12:08:48 +1100

    Remove legacy code

--- brainfuck/src/column.rs
+++
@@ -1,9 +0,0 @@
-// use algebra::Felt;
-
-// struct Column<E>(Vec<E>);
-
-// impl<E: Felt> Column<E> {
-//     fn interpolate(&self, randomizers: Vec<E>) -> Vec<E> {
-//         self.0
-//     }
-// }
